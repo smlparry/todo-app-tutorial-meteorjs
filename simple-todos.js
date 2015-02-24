5b1c61b8236988ffdf1f08ff9a973acc7bca623b
smlparry <samuel.parry@bigpond.com>
2015-02-24 23:41:55 +1100

    Finished tutorial

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,27 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  // This code only runs on the Client
-  Template.body.helpers({
-      tasks: function() {
-         return Tasks.find({}, { sort: {createdAt: -1} });
-      }
-  });
+
+    // Subscribe the user to a specific set of data
+    Meteor.subscribe('tasks');
+
+    Template.body.helpers({
+        tasks: function() {
+            if ( Session.get("hideCompleted") ) {
+                //   If hide completed is checked, filter tasks
+                return Tasks.find({ checked: {$ne: true} }, { sort: {createdAt: -1} });
+            } else {
+                // Otherwise return all of the tasks
+                return Tasks.find({}, { sort: {createdAt: -1} });
+            }
+        },
+        hideCompleted: function() {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function() {
+            return Tasks.find({ checked: {$ne: true}}).count();
+        }
+    });
 
     Template.body.events({
       "submit .new-task": function (event) {
@@ -14,10 +29,7 @@ if (Meteor.isClient) {
         var text = event.target.text.value;
 
         // Insert into the database
-        Tasks.insert({
-            text: text,
-            createdAt: new Date()
-        });
+        Meteor.call('addTask', text);
 
         // Clear form
         event.target.text.value = "";
@@ -26,14 +38,83 @@ if (Meteor.isClient) {
       }
     });
 
+    Template.task.helpers({
+        isOwner: function() {
+            return this.owner === Meteor.userId();
+        }
+    });
+
     Template.task.events({
         'click .toggle-checked': function() {
             //  Set the checked property to the opposite of its current value
-            Tasks.update(this._id, {$set: {checked: ! this.checked }});
+            Meteor.call('setChecked', this._id, ! this.checked);
         },
         'click .delete': function() {
             //   Delete the item from the list
-            Tasks.remove(this._id);
+            Meteor.call('deleteTask', this._id);
+        },
+        'change .hide-completed input': function(event) {
+            Session.set('hideCompleted', event.target.checked);
+        },
+        'click .toggle-private': function() {
+            Meteor.call('setPrivate', this._id, ! this.private);
         }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+}
+
+Meteor.methods({
+    addTask: function(text) {
+        // Make sure the user is logged in before inserting a task
+        if ( ! Meteor.userId() ) {
+            throw new Meteor.Error("not-authorized");
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+    deleteTask: function(taskId) {
+        var task = Tasks.findOne(taskId);
+
+        if ( task.private && task.owner !== Meteor.userId() ){
+            throw new Meteor.error('not-authorized');
+        }
+
+        Tasks.remove(taskId);
+    },
+    setChecked: function(taskId, setChecked) {
+        var task = Tasks.findOne(taskId);
+
+        if ( task.private && task.owner !== Meteor.userId() ) {
+            throw new Meteor.error('not-authorized');
+        }
+
+        Tasks.update(taskId, { $set: { checked: setChecked } });
+    },
+    setPrivate: function(taskId, setToPrivate) {
+        var task = Tasks.findOne(taskId);
+        // Make sure only the task owner can actually make the task private
+        if ( task.owner !== Meteor.userId() ) {
+            throw new Meteor.error("not-authorized");
+        }
+        Tasks.update(taskId, { $set: {private: setToPrivate} });
+    }
+});
+
+if (Meteor.isServer) {
+    Meteor.publish("tasks", function() {
+        return Tasks.find({
+            $or: [
+                { private: {$ne: true} },
+                { owner: this.userId }
+            ]
+        });
     });
 }
