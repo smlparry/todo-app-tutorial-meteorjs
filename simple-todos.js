afee242c0a6019c1f01e568878dd0e77d0256b96
smlparry <samuel.parry@bigpond.com>
2015-02-24 19:15:29 +1100

    Added database functionality

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,25 @@ if (Meteor.isClient) {
   // This code only runs on the Client
   Template.body.helpers({
       tasks: function() {
-         return Tasks.find({}); 
+         return Tasks.find({}, { sort: {createdAt: -1} });
       }
   });
+
+  Template.body.events({
+      "submit .new-task": function (event) {
+        //   This function is called when the new task form is submitted
+        var text = event.target.text.value;
+
+        // Insert into the database
+        Tasks.insert({
+            text: text,
+            createdAt: new Date()
+        });
+
+        // Clear form
+        event.target.text.value = "";
+        // Prevent default form submit
+        return false;
+      }
+  })
 }
